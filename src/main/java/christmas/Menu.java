50e0a56b6c98043590423c0725b1d8e05731e0dc
hyeonjilee <skylar12200@gmail.com>
2023-11-13 15:26:47 +0900

    feat(Menu) : 메뉴와 카테고리 항목 추가

--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -3,11 +3,18 @@ package christmas;
 import java.util.Arrays;
 
 public enum Menu {
-    MUSHROOM_SOUP("양송이수프", 6000, MenuCategory.MAIN),
-    TAPAS("타파스", 5500, MenuCategory.MAIN),
-
-    COKE_ZERO("제로콜라", 5500, MenuCategory.DRINK),
-    RED_WINE("레드와인", 5500, MenuCategory.DRINK);
+    MUSHROOM_SOUP("양송이수프", 6000, MenuCategory.APPETIZER),
+    TAPAS("타파스", 5500, MenuCategory.APPETIZER),
+    Caesar_Salad("시저샐러드", 8000, MenuCategory.APPETIZER),
+    TBONE_STEAK("티본스테이크", 55000, MenuCategory.MAIN),
+    BBQ_RIBS("바비큐립", 54000, MenuCategory.MAIN),
+    Seafood_PASTA("해산물파스타", 35000, MenuCategory.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, MenuCategory.MAIN),
+    CHOCOLATE_CAKE("초코케이크", 5000, MenuCategory.DESSERT),
+    ICECREAM("아이스크림", 5000, MenuCategory.DESSERT),
+    COKE_ZERO("제로콜라", 3000, MenuCategory.DRINK),
+    RED_WINE("레드와인", 60000, MenuCategory.DRINK),
+    CHAMPAGNE("샴페인", 250000, MenuCategory.DRINK);
 
     private final String name;
     private final int price;
--- src/main/java/christmas/MenuCategory.java
+++ src/main/java/christmas/MenuCategory.java
@@ -1,7 +1,9 @@
 package christmas;
 
 public enum MenuCategory {
+    APPETIZER("애피타이저"),
     MAIN("메인"),
+    DESSERT("디저트"),
     DRINK("음료");
 
     private final String name;
