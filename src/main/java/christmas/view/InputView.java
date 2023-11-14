d1b4f87fa78214f73f96c5bd3b9702f43aeb0a93
hyeonjilee <skylar12200@gmail.com>
2023-11-14 12:16:34 +0900

    fix (InputView) : 입력 앞,뒤의 공백 제거

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -25,14 +25,14 @@ public class InputView {
 
     public int inputDate() {
         printer.printLine("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String rawDate = reader.readLine();
+        String rawDate = reader.readLine().trim();
         validator.validateDate(rawDate);
         return Converter.convertToInt(rawDate);
     }
 
     public List<String> inputOrder() {
         printer.printLine("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String rawOrderGroup = reader.readLine();
+        String rawOrderGroup = reader.readLine().trim();
         validator.validateOrderGroup(rawOrderGroup);
         validator.validateOrder(Converter.split(ORDER_SEPARATOR, rawOrderGroup));
         validator.validateOrderCount(Converter.split(ORDER_SEPARATOR, rawOrderGroup));
